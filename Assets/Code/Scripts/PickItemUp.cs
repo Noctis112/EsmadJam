66ad3258855b55464180233ba1fa355e6eccec41
Noctis112 <80061959+Noctis112@users.noreply.github.com>
2023-02-24 23:00:21 +0000

    update pickup

--- Assets/Code/Scripts/PickItemUp.cs
+++ Assets/Code/Scripts/PickItemUp.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PickItemUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerInput _playerInput;
+    public float pickupDistance = 2.0f;
+
+    private GameObject heldItem = null;
+
+    private void OnEnable()
     {
-        
+        _playerInput = GetComponent<PlayerInput>();
+        pickupAction.action.performed += OnPickupAction;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        pickupAction.action.Disable();
+        pickupAction.action.performed -= OnPickupAction;
+    }
+
+    private void OnPickupAction(InputAction.CallbackContext context)
+    {
+        if (heldItem == null)
+        {
+            // Try to pick up an item
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, pickupDistance))
+            {
+                if (hit.collider.CompareTag("Pickup"))
+                {
+                    // Pick up the item
+                    heldItem = hit.collider.gameObject;
+                    heldItem.transform.parent = transform;
+                    heldItem.transform.localPosition = new Vector3(0.0f, 0.0f, pickupDistance);
+                    heldItem.GetComponent<Rigidbody>().isKinematic = true;
+                }
+            }
+        }
+        else
+        {
+            // Drop the held item
+            heldItem.transform.parent = null;
+            heldItem.GetComponent<Rigidbody>().isKinematic = false;
+            heldItem = null;
+        }
     }
 }
