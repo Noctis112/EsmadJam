2411769be78036c5ce7cd09afa20f9bc6b1c6495
Antonio Freitas <antoniofinisterrafreitas@gmail.com>
2023-02-25 00:01:57 +0000

    NavMeshUpdate

--- Assets/Code/Scripts/BossPlayerController.cs
+++ Assets/Code/Scripts/BossPlayerController.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossPlayerController : MonoBehaviour
 {
+    [SerializeField] GameObject DoorAPos;
+    [SerializeField] GameObject DoorCPos;
+
+    [SerializeField] NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            agent.SetDestination(DoorCPos.transform.position);
+        }
     }
 }
